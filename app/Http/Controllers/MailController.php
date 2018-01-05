4651e0385f7a50e2d6796c44ef8a707a2cdf7b3f
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-01-05 21:47:03 +0800

    發送mail的方法

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 
 class MailController extends Controller
 {
-    //
+    public function getSend()
+    {
+        $content = 'test';
+        $data = ['name'=>'許漢昌','content'=>$content,];
+        //寄出信件
+        Mail::send('email.test', $data, function($message) {
+            $message->subject('Laravel 5 Mail');
+            $message->from('f37c183652-b21d25@inbox.mailtrap.io', 'netadmin');
+            $message->to('a0952288480@gmail.com', '許漢昌');
+        });
+
+        return "Email 已寄出";
+    }
 }
